9045d4a254c5c7a3e4547f9843f1524b28417d1b
Naziullah Shawan <mshawn192106@bscse.uiu.ac.bd>
2021-06-27 19:52:07 +0600

    make pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-    <h1>This is retro blog site</h1>
+    <Home />
     </div>
   );
 }
