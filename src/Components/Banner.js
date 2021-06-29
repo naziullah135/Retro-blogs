bdf0ca75f60157865c6fe8f5ca029839abca9f2f
Naziullah Shawan <mshawn192106@bscse.uiu.ac.bd>
2021-06-29 10:28:23 +0600

    added navbar

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const Banner = () => {
     return (
         <div>
-            
+           
         </div>
     );
 };
 
-export default Login;
+export default Banner;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Retro Blogs</a>
+          </div>
+          <div class="d-flex">
+            <a class="navbar-brand" href="#">Login</a>
+          </div>
+        </nav>
+      </div>
+    );
+};
+
+export default Navbar;
